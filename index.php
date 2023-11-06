d8884f3271f7d289fcffdb1aa814537eb51dac75
Manu199 <manu199gh@gmail.com>
2023-11-06 15:37:41 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+class Movie {
+
+}
+
+$Avengers = new Movie();
+$Thor = new Movie();
+
+
+var_dump($Avengers);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP</title>
+</head>
+<body>
+
+    <h1>OOP</h1>
+
+</body>
+</html>
